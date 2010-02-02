c7673b9fc210d2e88005a495ea688eae22f43c2d
konrad.balys <konrad.balys@f23c21ba-0618-11df-87ec-d30288882566>
2010-02-02 15:44:05 +0000

    modyfikacje różne...

--- trunk/GuruTest/GuruTest/Default.aspx.cs
+++ trunk/GuruTest/GuruTest/Default.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
 
 namespace GuruTest
 {
@@ -11,15 +12,45 @@ namespace GuruTest
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //System.Web.Security.MembershipCreateStatus t;
-            string[] a = System.Web.Security.Roles.Provider.GetAllRoles();
-            string[] b = System.Web.Security.Roles.Provider.GetRolesForUser("f");
-            int i = 0;
+            //
+            //string[] a = System.Web.Security.Roles.Provider.GetAllRoles();
+            //string[] b = System.Web.Security.Roles.Provider.GetRolesForUser("f");
+            //int i = 0;
 
             //System.Web.Security.Membership.Provider.UnlockUser("f");
             //int a;
             //System.Web.Security.Membership.Provider.GetAllUsers(0, 100, out a);
+
+            //System.Web.Security.MembershipCreateStatus t;
             //System.Web.Security.Membership.Provider.CreateUser("f", "test!!!!", "f@f.f", "f?", "f!", false, null, out t);
+
+            string [] s = Roles.GetUsersInRole("CanLogOn");
+            s = Roles.GetUsersInRole("CanAddArticle");
+            s = Roles.GetUsersInRole("CanDoSomething");
+            s = Roles.GetUsersInRole("CanLogOut");
+
+            bool result = Roles.IsUserInRole("test", "CanLogOn");
+            result = Roles.IsUserInRole("test", "CanAddArticle");
+            result = Roles.IsUserInRole("test", "CanDoSomething");
+            result = Roles.IsUserInRole("test", "CanLogOut");
+
+            result = Roles.IsUserInRole("f", "CanLogOn");
+            result = Roles.IsUserInRole("f", "CanAddArticle");
+            result = Roles.IsUserInRole("f", "CanDoSomething");
+            result = Roles.IsUserInRole("f", "CanLogOut");
+
+            result = Roles.IsUserInRole("a", "CanLogOn");
+            result = Roles.IsUserInRole("f", "a");
+            result = Roles.IsUserInRole("y", "x");
+
+            s = Roles.FindUsersInRole("CanLogOn", "test");
+            s = Roles.FindUsersInRole("CanLogOn", "te%");
+            s = Roles.FindUsersInRole("CanLogOn", "%");
+
+            s = Roles.FindUsersInRole("CanDoSomething", "test");
+            s = Roles.FindUsersInRole("CanDoSomething", "te%");
+            s = Roles.FindUsersInRole("CanDoSomething", "%");
+            s = Roles.FindUsersInRole("a", "test");
         }
 
         protected void Button1_Click(object sender, EventArgs e)
